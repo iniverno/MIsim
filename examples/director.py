506abc607bbd0374178fc98a5adeaf19afc6a0fa
Jorge Albericio <jorge.albericio@gmail.com>
2015-10-14 15:20:26 -0400

    commits in both unit and director

--- examples/director.py
+++ examples/director.py
@@ -1,10 +1,35 @@
 import unit
 
-class director:
+class directorLayer:
   def __init__(self, n_units):
     units = []
     for i in range(n_units):
       units.append(Unit(i, 16, 16, 16, (1<<20)))
 
-  def initializeLayer():
-  
+  # this function copy the filter weights into the unit eDRAM
+  def initializeLayer(self, weights):
+    nUnits = len(units)
+    nTotalFilters = weights.shape[0]
+    #how many filters have to go to each unit
+    nFiltersPerUnit = nTotalFilters / nUnits
+    print "%d filters per unit" % (nFiltersPerUnit)
+    # if the total number of filters is not a multiple of nUnits
+    nAdditionalFilters = nTotalFilters - (nFiltersPerUnit * nUnits)
+    print "plus %d additional filters"%(nAdditionalFilters)
+    
+    for idxFilter in range(nTotalFilters):
+      self.units[idxFilter % nUnits].fill_NBin(weights, idxFilter)
+
+
+  def computeConvolutional(self, inputData):
+    # generate windows
+
+    # process each window
+
+  def processWindow(self, windowData):
+    # the window has to be processed by each unit
+    # divide the window in chunks as big as NBin
+    # TODO: in the baseline, all the units have to process every window
+    # the solution to deal with the new system and how it slices computation,
+    # will be to add clusterDirector that will take care of slicing the computation 
+    # among the narrow units conforming a cluster  
--- examples/unit.py
+++ examples/unit.py
@@ -6,25 +6,37 @@ class Unit:
   Ti = 16
   Tn = 16
 
-  def __init__(self, uid, NBin_num_entries, Ti, Tn, SB_size):
+  def __init__(self, verbose, uid, NBin_num_entries, Ti, Tn, SB_size):
     self.unit_id = uid
+    self.VERBOSE = verbose
     self.NBin_num_entries = NBin_num_entries
     self.Ti = Ti
     self.Tn = Tn
-    
+    self.busy = False
+
     #instance variables 
     self.SB_size = SB_size
+    self.SB_available = SB_size
     self.SB_data = {}
     self.NBin_data = np.zeros((Ti, NBin_num_entries))
     
 
   # TODO: what happens if the computation is broken in subbricks ????
-  def fill_SB(filter_data, filter_indexes):
-    filter_size = filter_data.shape[1]*filter_data.shape[2]*filter_data.shape[3] * len(filter_indexes)
-    assert filter_size <= SB_size, "The filters being loaded in SB of unit %d are too big (%d vs. %d available)"%(self.unit_id, filter_size, self.SB_size)
-    for f in filter_indexes:
-      SB_data[f] = filter_data[f]
+  def fill_SB(self, filter_data, filter_indexes):
+    filter_size = filter_data.shape[1]*filter_data.shape[2]*filter_data.shape[3] 
+    nFilters = len(filter_indexes)
 
-  def fill_NBin(input_data, offsets = []):
+    # Check if there is enough space
+    assert filter_size * nFilters <= SB_available, "The filters being loaded in SB of unit %d are too big (%d vs. %d available)"%(self.unit_id, filter_size, self.SB_size)
+
+    for f in filter_indexes:
+      if self.VERBOSE:
+        print "SB in unit %d is now storing filter #%d"%(unit_id, f)
+      self.SB_data[f] = filter_data[f]
+
+    # update the available space
+    self.SB_available -= filter_size * nFilters
+
+  def fill_NBin(self, input_data, offsets = []):
     assert offsets != [] or Ti !=1, "Something is wrong with the parameters of fill_NBin"
-    NBin_data = input_data 
+    self.NBin_data = input_data 
